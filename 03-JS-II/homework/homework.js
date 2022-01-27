808b854ccc8c659f348d0f101857e566510c5a41
FlavioDarioo <flavioballestrin.hys@gmail.com>
2022-01-27 17:19:33 -0300

    JS2 va queriendo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -177,8 +177,12 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if (valor === true) {
+    return "Soy verdadero"
+  };
+    return "Soy falso";
+    }
 
-}
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
